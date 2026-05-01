cf5f7cf063c7b86afddb1c74d88b8f52884ef1b3
MatinHabi <161264948+MatinHabi@users.noreply.github.com>
2026-05-01 21:07:28 +1000

    Add: find-lucky-integer-in-an-array - submission-2

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        vector<int>t(arr.size(),0);
+        bool zero = false;
+        for(int& i : arr){
+            if(i <= arr.size()){
+                t[i-1]++;
+            }
+        }
+        int res = -1;
+        for(int i = 0 ; i < t.size() ; i++){
+            if(t[i] == i+1){
+                res = max(res, t[i]);
+            }
+        }
+        cout << "\n";
+        return res;
+    }
+};
